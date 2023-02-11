9d9570fce5acbe45a7cf6fd4bae1e90db8dc03f6
Nilton Freitas <nilton.freitas@ironhack.com>
2023-02-11 16:15:08 -0300

    Iteration 4

--- routes/todos.routes.js
+++ routes/todos.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import Todo from '../models/Todo.model.js'
+import User from '../models/User.model.js'
 import isAuthenticatedMiddleware from '../middlewares/isAuthenticatedMiddleware.js'
 
 const todosRouter = Router()
 
-todosRouter.get('/', async (req, res) => {
+todosRouter.get('/', isAuthenticatedMiddleware, async (req, res) => {
     try {
-        const todos = await Todo.find()
+        const todos = await Todo.find({user: req.user.id})
         return res.status(200).json(todos)      
     } catch (error) {
         console.log(error)
@@ -16,10 +17,18 @@ todosRouter.get('/', async (req, res) => {
 
 
 todosRouter.post('/', isAuthenticatedMiddleware, async (req, res) => {
-    const payload = req.body
+    //Adicionamos ao payload o id do usuário que vem do req.user gerado no middleware de autenticação
+    const payload = { ...req.body, user: req.user.id }
 
     try {
         const newTodo = await Todo.create(payload)
+
+        //Atualizar o usuário dono do todo para incluir o id do todo criado
+        //Achar o usuário
+        //Atualizar o usuário
+        ////Inserir o id do novo todo
+        await User.findOneAndUpdate({_id: req.user.id}, {$push: {todos: newTodo._id }})
+
         return res.status(201).json(newTodo)      
     } catch (error) {
         console.log(error)
@@ -27,11 +36,15 @@ todosRouter.post('/', isAuthenticatedMiddleware, async (req, res) => {
     }
 })
 
-todosRouter.put('/:id', async (req, res) => {
+todosRouter.put('/:id', isAuthenticatedMiddleware, async (req, res) => {
     const { id } = req.params
     const payload = req.body
     try {
-        const updatedTodo = await Todo.findOneAndUpdate({_id: id}, payload, { new: true })
+        const updatedTodo = await Todo.findOneAndUpdate(
+            {_id: id, user: req.user.id}, 
+            payload, 
+            { new: true }
+        )
         return res.status(200).json(updatedTodo)      
     } catch (error) {
         console.log(error)
@@ -39,10 +52,10 @@ todosRouter.put('/:id', async (req, res) => {
     }
 })
 
-todosRouter.delete('/:id', async (req, res) => {
+todosRouter.delete('/:id', isAuthenticatedMiddleware, async (req, res) => {
     const { id } = req.params
     try {
-        await Todo.findOneAndDelete({_id: id})
+        await Todo.findOneAndDelete({_id: id, user: req.user.id})
         return res.status(204).json()      
     } catch (error) {
         console.log(error)
